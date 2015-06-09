767574f531c7665075a0cfa0e3d74396ad2b1b90
Ash McKenzie <ash@the-rebellion.net>
2015-06-09 12:39:25 +1000

    checkpoint

--- demo-common/base/scripts/dnsmasq_daemon.sh
+++ demo-common/base/scripts/dnsmasq_daemon.sh
@@ -2,6 +2,6 @@
 
 . /etc/environment
 
-echo "server=/consul/${CONSUL_PORT_53_UDP_ADDR}#53" > /etc/dnsmasq.d/10-consul
+echo "server=/consul/${DEMO_COMMON_CONSUL_PORT_53_UDP_ADDR}#53" > /etc/dnsmasq.d/10-consul
 
-dnsmasq --no-daemon 2>&1
+/usr/sbin/dnsmasq --no-daemon 2>&1
